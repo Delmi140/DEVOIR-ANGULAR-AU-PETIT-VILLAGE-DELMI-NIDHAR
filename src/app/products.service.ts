8ca820980b921490546e822b01b7cf9a4d3d25bc
Delmi140 <156790899+Delmi140@users.noreply.github.com>
2024-06-20 13:28:58 +0200

    Add Home Component

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -6,12 +6,12 @@ import { Injectable } from '@angular/core';
 export class ProductsService {
 
   products = [
-    {id:1 , name: "Figurine Astérix ", description: 'À propos de cet article :' , price: 12.99 , size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021'},
-    {id:2, name: "Figurine Obelix ", description: 'À propos de cet article: ' , price: 13.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021'},
-    {id:3, name: "Figurine Bonnemine ", description: 'À propos de cet article :' , price: 14.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021'},
-    {id:4, name: "Figurine Cesar ", description: 'À propos de cet article :' , price: 9.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021'},
-    {id:5, name: "Figurine Panoramix ", description: 'À propos de cet article :' , price:16.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021'},
-    {id:6, name: "Figurine Pirate Barbe Rouge", description: 'À propos de cet article :' , price:19.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021'}
+    {id:1 , name: "Figurine Astérix ", description: 'À propos de cet article :' , price: 12.99 , size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021',imageUrl :'../assets/image/figurine Astérix .jpg'},
+    {id:2, name: "Figurine Obelix ", description: 'À propos de cet article: ' , price: 13.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021',imageUrl :'../assets/image/figurine obelix.jpg'},
+    {id:3, name: "Figurine Bonnemine ", description: 'À propos de cet article :' , price: 14.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021',imageUrl :'../assets/image/Figurine-Bonnemine.jpg'},
+    {id:4, name: "Figurine Cesar ", description: 'À propos de cet article :' , price: 9.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021', imageUrl :'../assets/image/Figurine-Cesar.jpg'},
+    {id:5, name: "Figurine Panoramix ", description: 'À propos de cet article :' , price:16.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021', imageUrl :'../assets/image/Figurine-Panoramix.jpg'},
+    {id:6, name: "Figurine Pirate Barbe Rouge", description: 'À propos de cet article :' , price:19.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021', imageUrl :'../assets/image/Figurine-Pirate Barbe Rouge .jpg'}
   ]
 
 
