e0746a16d949cd8d9e78644a7481eb53f4c7b0b2
Delmi140 <156790899+Delmi140@users.noreply.github.com>
2024-06-20 20:32:41 +0200

    Add Header and home

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ProductsService {
 
   products = [
-    {id:1 , name: "Figurine Astérix ", description: 'À propos de cet article :' , price: 12.99 , size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021',imageUrl :'../assets/image/figurine Astérix .jpg'},
+    {id:1 , name: "Figurine Astérix ", description: 'À propos de cet article :' , price: 12.99 , size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021',imageUrl :'../assets/image/figurine Astérix .jpg',descriptionCard:'Retrouvez ici Obélix, de la célèbre série Astérix et Obélix par Uderzo et Goscinny.Format : 9 x 19 X 12 cm, fabriquée en résine.'},
     {id:2, name: "Figurine Obelix ", description: 'À propos de cet article: ' , price: 13.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021',imageUrl :'../assets/image/figurine obelix.jpg'},
     {id:3, name: "Figurine Bonnemine ", description: 'À propos de cet article :' , price: 14.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021',imageUrl :'../assets/image/Figurine-Bonnemine.jpg'},
     {id:4, name: "Figurine Cesar ", description: 'À propos de cet article :' , price: 9.99 ,size : 'Hauteur:23,5 cm ',weight : 'Poids: 300g', matter:'Matière :Résine', madein:'Fabriqué en :France',year:'Année : 2021', imageUrl :'../assets/image/Figurine-Cesar.jpg'},
